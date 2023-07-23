fd564d1c73522db096d1142a8248edce3ceb1225
Imad-AHDDAD <imadahddad02@gmail.com>
2023-07-23 02:27:37 +0100

    feature: app initialization

---
+++ app/src/main/java/com/a7dev/moststarredgithubrepos_cleanarchitecture/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.a7dev.moststarredgithubrepos_cleanarchitecture
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
